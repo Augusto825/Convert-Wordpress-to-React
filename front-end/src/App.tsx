f4bb04339f5e1c0ffe4a78f16091044567cfd87b
Augusto825 <guto.neto.dev@gmail.com>
2024-09-20 14:44:02 +0900

    complete 3 pages(interfacepage, UI, webdesign)

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -11,6 +11,13 @@ import WebsiteConstruction from "./pages/FooterPages/WebsiteConstructionPage";
 import BuildingImageWebsitePage from "./pages/FooterPages/BuildingImageWebsitePage";
 import BuildTradingSite from "./pages/FooterPages/BuildTradingSite";
 import BuildPortalSite from "./pages/FooterPages/BuildPortalSite";
+import WebDesignPage from "./pages/FooterPages/WebDesignPage";
+import UIdesignPage from "./pages/FooterPages/UIdesignPage";
+import InterfaceDesignPage from "./pages/FooterPages/InterfaceDesignPage";
+import AppDesignPage from "./pages/FooterPages/AppDesignPage";
+import FacebookPromotionPage from "./pages/FooterPages/FacebookPromotionPage";
+import InstagramDesignPage from "./pages/FooterPages/InstagramDesignPage";
+import TiktokPromotionPage from "./pages/FooterPages/TiktokPromotionPage";
 
 
 const App: React.FC = () => {
@@ -28,13 +35,13 @@ const App: React.FC = () => {
           <Route path="/בניית אתר תדמית"element={<BuildingImageWebsitePage />} />
           <Route path="/בניית אתר מסחר" element={<BuildTradingSite/>} />
           <Route path="/בניית אתר פורטל" element={<BuildPortalSite />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/" element={<HomePage />} />
+          <Route path="/עיצוב אתרים" element={<WebDesignPage />} />
+          <Route path="/עיצוב UI/UX" element={<UIdesignPage />} />
+          <Route path="/עיצוב ממשק משתמש" element={<InterfaceDesignPage />} />
+          <Route path="/עיצוב אפליקציות" element={<AppDesignPage />} />
+          <Route path="/קידום ב-Facebook" element={<FacebookPromotionPage />} />
+          <Route path="/קידום ב-Instagram" element={<InstagramDesignPage />} />
+          <Route path="/קידום ב-Tik Tok" element={<TiktokPromotionPage />} />
         </Routes>
       </MainLayout>
     </Router>
--- front-end/src/components/layoutComponents/Footer/FooterComponents/FooterLinks.tsx
+++ front-end/src/components/layoutComponents/Footer/FooterComponents/FooterLinks.tsx
@@ -7,17 +7,17 @@ const footerData = [
   {
     title: "קידום אתרים ודיגיטל",
     items: [
-      {title: "קידום ב-Facebook", url: '/קידום%20ב-Facebook'},
-      {title: "קידום ב-Instagram", url: '/קידום%20ב-Instagram'},
-      {title: "קידום ב-Tik Tok", url: '/קידום%20ב-Tik%20Tok'},],
+      {title: "קידום ב-Facebook", url: "קידום ב-Facebook"},
+      {title: "קידום ב-Instagram", url: "קידום ב-Instagram"},
+      {title: "קידום ב-Tik Tok", url: "קידום ב-Tik Tok"},],
   },
   {
     title: "עיצוב אתרים",
     items: [
-      {title: "עיצוב אתרים", url: '/עיצוב%20אתרים'},
-      {title: "עיצוב UI/UX", url: '/%d7%a2%d7%99%d7%a6%d7%95%d7%91-ui-ux/'},
-      {title: "עיצוב ממשק משתמש", url: '/עיצוב%20ממשק%20משתמש'},
-      {title: "עיצוב אפליקציות", url: '/עיצוב%20אפליקציות%20'},
+      {title: "עיצוב אתרים", url: "עיצוב אתרים"},
+      {title: "עיצוב UI/UX", url: "עיצוב UI/UX"},
+      {title: "עיצוב ממשק משתמש", url: "עיצוב ממשק משתמש"},
+      {title: "עיצוב אפליקציות", url: "עיצוב אפליקציות"},
     ],
   },  
   {
