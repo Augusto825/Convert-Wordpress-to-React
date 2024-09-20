cd190994a0b7300a9e220bcdb345fceac6c6115d
Augusto825 <guto.neto.dev@gmail.com>
2024-09-21 05:51:35 +0900

    a bit fix

--- start.bat
+++ start.bat
@@ -4,7 +4,7 @@ set ROOT_DIR=%~dp0
 
 :: Navigate to the backend folder and start the backend server
 cd /d "%ROOT_DIR%%backend"
-start cmd/k "npm start"
+start cmd /k "npm start"
 
 :: Navigate to the frontend folder and start the frontend server
 cd /d "%ROOT_DIR%frontend"
